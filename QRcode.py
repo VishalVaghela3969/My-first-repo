97b38ee11ef92c852bd3a7d70db429c374b58362
Vishal Vaghela <vishalvaghela3969@gmail.com>
2023-07-05 12:56:01 +0530

    QR

---
+++ QRcode.py
@@ -0,0 +1,22 @@
+import qrcode
+
+# Text or data you want to encode in the QR code
+data = "hi Vishal"
+
+# Generate QR code instance
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+
+# Add data to the QR code
+qr.add_data(data)
+qr.make(fit=True)
+
+# Create an image from the QR code instance
+img = qr.make_image(fill_color="black", back_color="white")
+
+# Save the image file
+img.save("qr_code.png")
